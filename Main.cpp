7a35e45589dce49a36d870e8d00d2cdf35504aec
Christian Martínez <christt105@gmail.com>
2018-03-04 11:06:04 +0100

    square can shot but you can't push 2 key at the same time

--- Main.cpp
+++ Main.cpp
@@ -8,9 +8,13 @@ int main(int argc, char* argv[]) {
 	int a = 1;
 	SDL_Window* window;
 	SDL_Rect rect;
+	SDL_Rect shot;
 	SDL_Renderer* renderer;
 	rect.w = 50;
 	rect.h = 30;
+	shot.w = 40;
+	shot.h = 5;
+	shot.x = 501;
 	SDL_Event event;
 	window = SDL_CreateWindow(
 		"Awesomw Game",
@@ -20,25 +24,40 @@ int main(int argc, char* argv[]) {
 		500,
 		0
 	);
-	int x = 250;
-	int y = 250;
-
+	int x=250;
+	int y=250;
 	renderer = SDL_CreateRenderer(window, -1, 0);
-	while (a == 1) {
+	while(a==1) {
 		rect.x = x;
 		rect.y = y;
-		while (SDL_PollEvent(&event) != 0) {
-			if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+		if (SDL_PollEvent(&event) != 0) {
+			if (event.window.event == SDL_WINDOWEVENT_CLOSE || event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
 				a = 0;
-				SDL_DestroyWindow;
+				SDL_DestroyWindow(window);
 			}
-				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-				SDL_RenderClear(renderer);
-				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-				SDL_RenderFillRect(renderer, &rect);
-				
-				SDL_RenderPresent(renderer);
-		//SDL_Delay(10);
+			else{
+				if (event.key.keysym.scancode == SDL_SCANCODE_DOWN)
+					y = y + 5;
+				if (event.key.keysym.scancode == SDL_SCANCODE_UP)
+					y = y - 5;
+				if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+					x = x + 5;
+				if (event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+					x = x - 5;
+				if (event.key.keysym.scancode == SDL_SCANCODE_SPACE) {
+					shot.x = rect.x + 25;
+					shot.y = rect.y + 15;
+				}	
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderFillRect(renderer, &shot);
+		shot.x++;
+		SDL_RenderPresent(renderer);
 	}
 	SDL_Quit();
 	return 0;
