9604a350f0f2d5eabeba5e96725b336723ccda21
Christian Martínez <christt105@gmail.com>
2018-03-04 11:07:42 +0100

    change x86 lib name in order to github upload it

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 #include "SDL/include/SDL.h"
 
-#pragma comment (lib, "SDL/x86/SDL2.lib")
-#pragma comment (lib, "SDL/x86/SDL2main.lib")
+#pragma comment (lib, "SDL/libx86/SDL2.lib")
+#pragma comment (lib, "SDL/libx86/SDL2main.lib")
 
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
