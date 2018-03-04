0f0f4b3fb308b84bf6edf2a1bc8cdcc53587d780
Christian Martínez <christt105@gmail.com>
2018-03-04 09:46:23 +0100

    blue window and red rectangle

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,13 @@
 
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	int a = 1;
 	SDL_Window* window;
+	SDL_Rect rect;
+	SDL_Renderer* renderer;
+	rect.w = 50;
+	rect.h = 30;
+	//SDL_Event event;
 	window = SDL_CreateWindow(
 		"Awesomw Game",
 		SDL_WINDOWPOS_CENTERED,
@@ -14,8 +20,21 @@ int main(int argc, char* argv[]) {
 		500,
 		0
 	);
+	int x = 250;
+	int y = 250;
 
-	
-	SDL_Quit();
-	return 0;
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	while (a == 1) {
+		rect.x = x;
+		rect.y = y;
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+
+		//SDL_Delay(10);
+	}
+		SDL_Quit();
+		return 0;
 }
