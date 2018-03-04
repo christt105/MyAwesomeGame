395348314e59e95064388f7dfb7f0dc4fbd1834e
Christian Martínez <christt105@gmail.com>
2018-03-04 09:41:37 +0100

    Window created

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,16 @@
 
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	
+	SDL_Window* window;
+	window = SDL_CreateWindow(
+		"Awesomw Game",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		500,
+		500,
+		0
+	);
+
 	
 	SDL_Quit();
 	return 0;
