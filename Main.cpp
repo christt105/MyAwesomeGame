3c37b62ca9c15dbc36fa74f325dd5ad0c2e08e64
Christian Martínez <christt105@gmail.com>
2018-03-05 23:38:36 +0100

    textures + code

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@
 #pragma comment (lib, "SDL/SDL_Image/libx86/SDL2_image.lib")
 
 int main(int argc, char* argv[]) {
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	IMG_Init(IMG_INIT_PNG);
 	int a = 1;
@@ -13,23 +14,39 @@ int main(int argc, char* argv[]) {
 	SDL_Rect rect;
 	SDL_Rect shot;
 	SDL_Renderer* renderer;
-	rect.w = 50;
-	rect.h = 30;
-	shot.w = 40;
-	shot.h = 5;
+	SDL_Surface* goku;
+	goku = IMG_Load("goku.png");
+	SDL_Surface* background;
+	background = IMG_Load("minecraft.png");
+	SDL_Texture* back_tx;
+	SDL_Surface* shoot;
+	shoot = IMG_Load("hellokitty.png");
+	
+	rect.w = 793/5;
+	rect.h = 1007/5;
+	shot.w = 50;
+	shot.h = 50;
 	SDL_Event event;
 	const Uint8 *state = SDL_GetKeyboardState(NULL);
 	window = SDL_CreateWindow(
 		"Awesomw Game",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		500,
-		500,
-		0
+		1280,
+		720,
+		SDL_WINDOW_OPENGL
 	);
-	int x=250;
-	int y=250;
-	renderer = SDL_CreateRenderer(window, -1, 0);
+	int x=100;
+	int y=100;
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Texture* goku_tx = SDL_CreateTextureFromSurface(renderer, goku);
+	back_tx = SDL_CreateTextureFromSurface(renderer, background);
+	SDL_Texture* shoot_tx = SDL_CreateTextureFromSurface(renderer, shoot);
+	bool movUp = true;
+	bool movDown = true;
+	bool movLeft = true;
+	bool movRight = true;
+
 	while(a==1) {
 		rect.x = x;
 		rect.y = y;
@@ -40,7 +57,43 @@ int main(int argc, char* argv[]) {
 				SDL_DestroyWindow(window);
 			}
 			else{
-				if (state[SDL_SCANCODE_DOWN])
+				if (event.key.type == SDL_KEYDOWN) {
+					switch (event.key.keysym.scancode) {
+					case SDL_SCANCODE_UP:
+						movUp = true;
+						break;
+					case SDL_SCANCODE_RIGHT:
+						movRight = true;
+						break;
+					case SDL_SCANCODE_DOWN:
+						movDown = true;
+						break;
+					case SDL_SCANCODE_LEFT:
+						movLeft = true;
+						break;
+					}
+				}
+				else if (event.key.type == SDL_KEYUP) {
+					switch (event.key.keysym.scancode) {
+					case SDL_SCANCODE_UP:
+						isMoving = false;
+						break;
+					case SDL_SCANCODE_RIGHT:
+						rect.x += 5;
+						break;
+					case SDL_SCANCODE_DOWN:
+						rect.y += 5;
+						break;
+					case SDL_SCANCODE_LEFT:
+						rect.x -= 5;
+						break;
+					}
+				}
+				rect.y -= 5;
+				rect.x += 5;
+				rect.y += 5;
+				rect.x -= 5;
+				/*if (state[SDL_SCANCODE_DOWN])
 					y = y + 5;
 				if (state[SDL_SCANCODE_UP])
 					y = y - 5;
@@ -51,16 +104,23 @@ int main(int argc, char* argv[]) {
 				if (state[SDL_SCANCODE_SPACE]) {
 					shot.x = rect.x + 25;
 					shot.y = rect.y + 15;
-				}	
+				}	*/
 			}
 		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderFillRect(renderer, &rect);
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-		SDL_RenderFillRect(renderer, &shot);
+		SDL_RenderCopy(renderer, back_tx, 0, 0);
+		SDL_RenderCopy(renderer, goku_tx, 0, &rect);
+		SDL_RenderCopy(renderer, shoot_tx, 0, &shot);
+		
+		//SDL_RenderFillRect(renderer, &rect);
+		//SDL_RenderClear(renderer);
+		//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		
+		//SDL_RenderFillRect(renderer, &rect);
+		//SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		//SDL_RenderFillRect(renderer, &shot);
 		shot.x++;
+		
 		SDL_RenderPresent(renderer);
 	}
 	IMG_Quit();
