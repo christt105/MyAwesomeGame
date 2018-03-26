204b78fd7d3929149295140dd2cb9d341c7c2ed7
Christian Martínez <christt105@gmail.com>
2018-03-26 22:41:36 +0200

    Revert "can shoot multimples shots"
    This reverts commit 850d8b8012ee1d551ece1143d6890967db7c82a0.

--- Main.cpp
+++ Main.cpp
@@ -10,11 +10,9 @@ int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	IMG_Init(IMG_INIT_PNG);
 	int a = 1;
-	int contshot = 0;
 	SDL_Window* window;
 	SDL_Rect rect;
-	SDL_Rect shot[50];
-	SDL_Rect shootHK;
+	SDL_Rect shot;
 	SDL_Renderer* renderer;
 	SDL_Surface* goku;
 	goku = IMG_Load("goku.png");
@@ -28,20 +26,20 @@ int main(int argc, char* argv[]) {
 	rect.h = 1007/5;
 	rect.x = 50;
 	rect.y = 50;
-	for (int i = 0; i < 50; i++) {
-		shot[i].w = 50;
-		shot[i].h = 50;
-	}
+	shot.w = 50;
+	shot.h = 50;
 	SDL_Event event;
 	const Uint8 *state = SDL_GetKeyboardState(NULL);
 	window = SDL_CreateWindow(
 		"Awesomw Game",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		1920,
-		1080,
+		1280,
+		720,
 		SDL_WINDOW_OPENGL
 	);
+	int x=100;
+	int y=100;
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_Texture* goku_tx = SDL_CreateTextureFromSurface(renderer, goku);
 	back_tx = SDL_CreateTextureFromSurface(renderer, background);
@@ -50,13 +48,15 @@ int main(int argc, char* argv[]) {
 	bool movDown = false;
 	bool movLeft = false;
 	bool movRight = false;
-	bool movShot = false;
 
 	while(a==1) {
+		//rect.x = x;
+		//rect.y = y;
+		
 		if (SDL_PollEvent(&event) != 0) {
 			if (event.window.event == SDL_WINDOWEVENT_CLOSE || event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
 				a = 0;
-
+				SDL_DestroyWindow(window);
 			}
 			else{
 				if (event.key.type == SDL_KEYDOWN) {
@@ -72,9 +72,6 @@ int main(int argc, char* argv[]) {
 						break;
 					case SDL_SCANCODE_LEFT:
 						movLeft = true;
-						break;
-					case SDL_SCANCODE_SPACE:
-						movShot = true;
 						break;
 					}
 				}
@@ -94,36 +91,49 @@ int main(int argc, char* argv[]) {
 						break;
 					}
 				}
+				
+				/*if (state[SDL_SCANCODE_DOWN])
+					y = y + 5;
+				if (state[SDL_SCANCODE_UP])
+					y = y - 5;
+				if (state[SDL_SCANCODE_RIGHT])
+					x = x + 5;
+				if (state[SDL_SCANCODE_LEFT])
+					x = x - 5;
+				if (state[SDL_SCANCODE_SPACE]) {
+					shot.x = rect.x + 25;
+					shot.y = rect.y + 15;
+				}	*/
 			}
 		}
-		if (movUp)
+		if (movUp == true) {
 			rect.y--;
-		if (movDown)
-			rect.y++;
-		if (movLeft)
-			rect.x--;
-		if (movRight)
-			rect.x++;
-		if (movShot) {
-			shot[contshot].x = rect.x;
-			shot[contshot].y = rect.y;
-			contshot++;
-			if (contshot > 50)contshot = 0;
-			movShot = false;
 		}
-		
+		if (movDown == true) {
+			rect.y++;
+		}
+		if (movLeft == true) {
+			rect.x--;
+		}
+		if (movRight == true) {
+			rect.x++;
+		}
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, back_tx, 0, 0);
 		SDL_RenderCopy(renderer, goku_tx, 0, &rect);
-		for (int i = 0; i < contshot; i++) {
-			shootHK = shot[i];
-			SDL_RenderCopy(renderer, shoot_tx, 0, &shootHK);
-			shot[i].x++;
-		}
+		SDL_RenderCopy(renderer, shoot_tx, 0, &shot);
+		
+		//SDL_RenderFillRect(renderer, &rect);
+		//SDL_RenderClear(renderer);
+		//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		
+		//SDL_RenderFillRect(renderer, &rect);
+		//SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		//SDL_RenderFillRect(renderer, &shot);
+		shot.x++;
 		
 		SDL_RenderPresent(renderer);
 	}
-	SDL_DestroyWindow(window);
 	IMG_Quit();
 	SDL_Quit();
 	return 0;
